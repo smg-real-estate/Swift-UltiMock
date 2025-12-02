a92ced2f7f9df76bb9b0210d9c1fe05ab4d1e29e
Nickolay Tarbaiev <mykola.tarbaiev@homegate.ch>
2025-12-02 14:55:50 +0100

    48

--- Sources/SyntaxParser/MockType.swift
+++ Sources/SyntaxParser/MockType.swift
@@ -11,7 +11,50 @@ struct MockType {
         let declaration: FunctionDeclSyntax
 
         var stubIdentifier: String {
-            ""
+            var parts: [String] = []
+            var name = declaration.name.text.replacingOccurrences(of: "`", with: "")
+
+            let parameters = declaration.signature.parameterClause.parameters
+            let isAsync = declaration.signature.effectSpecifiers?.asyncSpecifier != nil
+
+            if isAsync && !parameters.isEmpty {
+                name += "_async"
+            }
+            parts.append(name)
+
+            for param in parameters {
+                let label = param.firstName.text
+                let typeName = param.type.description.trimmingCharacters(in: .whitespaces)
+
+                if label == "_" {
+                    let paramName = param.secondName?.text ?? ""
+                    if paramName == "anonymous" {
+                        parts.append("_\(typeName)")
+                    } else {
+                        parts.append("_\(paramName)_\(typeName)")
+                    }
+                } else {
+                    parts.append("\(label)_\(typeName)")
+                }
+            }
+
+            let returnTypeString = declaration.signature.returnClause?.type.description.trimmingCharacters(in: .whitespaces) ?? "Void"
+
+            if isAsync {
+                parts.append("async")
+            } else {
+                if returnTypeString == "Void" {
+                    parts.append("sync")
+                }
+            }
+
+            if declaration.signature.effectSpecifiers?.throwsSpecifier != nil {
+                parts.append("throws")
+            }
+
+            parts.append("ret_\(returnTypeString)")
+
+            return parts.joined(separator: "_")
         }
     }
 }
