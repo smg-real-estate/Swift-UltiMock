fd9576db6b2f7e56d7b3178af7073b2638b2f115
Nickolay Tarbaiev <mykola.tarbaiev@homegate.ch>
2025-12-02 15:35:22 +0100

    50

--- Sources/SyntaxParser/MockType.swift
+++ Sources/SyntaxParser/MockType.swift
@@ -3,7 +3,22 @@ import SwiftSyntax
 struct MockType {
     let declaration: DeclSyntax
 
-    init(_ typeInfo: Syntax.TypeInfo) {
+    init?(_ typeInfo: Syntax.TypeInfo, annotationKeys: [String] = ["sourcery", "UltiMock"]) {
+        let hasAnnotation = typeInfo.declaration.leadingTrivia.contains { piece in
+            if case .lineComment(let comment) = piece {
+                return annotationKeys.contains { key in
+                    comment.contains("\(key):AutoMockable")
+                }
+            }
+            if case .blockComment(let comment) = piece {
+                return annotationKeys.contains { key in
+                    comment.contains("\(key):AutoMockable")
+                }
+            }
+            return false
+        }
+
+        guard hasAnnotation else { return nil }
         self.declaration = typeInfo.declaration
     }
 
---
+++ Tests/SyntaxParserTests/MockTypeTests.swift
@@ -0,0 +1,54 @@
+//
+//  Test.swift
+//  UltiMock
+//
+//  Created by Mykola Tarbaiev on 02.12.25.
+//
+
+import Testing
+@testable import SyntaxParser
+import SwiftParser
+import SwiftSyntax
+
+struct MockTypeTests {
+    @Test func `init fails without mock annotation comment`() async throws {
+        let source = Parser.parse(source: """
+        // This is a regular comment
+        protocol MyProtocol {
+            func doSomething() {}
+        }
+        """).statements.first?.item
+
+        let declaration = try #require(ProtocolDeclSyntax(source))
+
+        let typeInfo = Syntax.TypeInfo(
+            scope: [],
+            declaration: DeclSyntax(declaration)
+        )
+
+        let mockType = MockType(typeInfo)
+
+        #expect(mockType == nil)
+    }
+
+    @Test(arguments: ["foo", "bar"])
+    func `init succeeds when type has matching annotation`(annotationKey: String) async throws {
+        let source = Parser.parse(source: """
+        // \(annotationKey):AutoMockable
+        protocol MyProtocol {
+            func doSomething() {}
+        }
+        """).statements.first?.item
+
+        let declaration = try #require(ProtocolDeclSyntax(source))
+
+        let typeInfo = Syntax.TypeInfo(
+            scope: [],
+            declaration: DeclSyntax(declaration)
+        )
+
+        let mockType = MockType(typeInfo, annotationKeys: ["foo", "bar"])
+
+        #expect(mockType != nil)
+    }
+}
